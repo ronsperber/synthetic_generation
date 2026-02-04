11cde379977af1f2c97453873300ff861774d478
ronsperber <85753940+ronsperber@users.noreply.github.com>
2026-02-04 11:36:20 -0500

    added docstrings to ganprocess.

--- src/synthetic_generation/gan/process.py
+++ src/synthetic_generation/gan/process.py
@@ -1,19 +1,42 @@
+"""
+module that holds GAN processes (Classes to hold models and train/save)
+"""
 from typing import Type, Callable
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from .models import Generator, Discriminator
 from .training import train_gan, train_wgan_gp 
 from .model_saving import load_gan_checkpoint, save_gan_checkpoint
 
 class BaseGanProcess:
+    """
+    Base class for GAN processes
+    """
     def __init__(self, G:Generator, D:Discriminator):
+        """
+        Create process with Generator and Discriminator/Critic
+        G : Generator
+            Generator model
+        D: Discriminator
+            Discriminator/Critic model
+        """
         self.G = G
         self.D = D
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.G.to(self.device)
         self.D.to(self.device)
 
-    def generate_samples(self, num_samples, c=None):
+    def generate_samples(self, num_samples: int, c: torch.Tensor | None = None) -> torch.Tensor:
+        """
+        Generate samples from model
+        Parameters
+        ---------
+        num_samples : int
+            number of samples to produce
+        c: torch.Tensor | None
+            when the model uses a conditional, conditional used to create samples
+        """
         with torch.no_grad():
             return self.G.generate_samples(num_samples=num_samples, c=c).cpu()
 
@@ -22,6 +45,15 @@ class BaseGanProcess:
             path: str,
             training_configs : dict | None = None
     ):
+        """
+        saving a process
+        Parameters
+        ----------
+        path: str
+            location to save
+        training_configs: dict | None
+            optional dictionary of training configs
+        """
         save_gan_checkpoint(save_path=path, G=self.G, D=self.D, training_configs=training_configs)
 
     @classmethod
@@ -31,6 +63,17 @@ class BaseGanProcess:
              D_cls: Type[nn.Module] = Discriminator,
              map_location: Callable | str | dict | None = None
     ):
+        """
+        class method to create a class from a saved checkpoint
+        path : str
+            where to load the class from
+        G_cls : Type[nn.Module]:
+            class for the Generator
+        D_cls : Type[nn.Module]:
+            class for the Discriminator/Critic
+        map_location: Callable | str | dict | None
+            information used for location for torch.load
+        """
         G,D,_ = load_gan_checkpoint(
             path=path,
             generator_cls=G_cls,
@@ -40,12 +83,40 @@ class BaseGanProcess:
         return cls(G, D)
         
 class GANProcess(BaseGanProcess):
-    def train(self, X: torch.Tensor, c: torch.Tensor | None = None, **train_args):
+    """
+    class for GAN process
+    """
+    def train(self, X: torch.Tensor | DataLoader, c: torch.Tensor | None = None, **train_args):
+        """
+        method to train the process on given data
+        and storing the training configs
+
+        Parameters
+        ----------
+        X: torch.Tensor | DataLoader
+            data to be trained on
+        c : torch.Tensor | None
+            optional conditional tensor
+        **train_args:
+            arguments used for training
+        """
         self.train_configs = train_gan(G=self.G, D=self.D, X=X, c=c, return_configs=True, **train_args)
-    def train_save(self, save_path: str, X: torch.Tensor,c: torch.Tensor | None = None, **train_args):
+
+    def train_save(self, save_path: str, X: torch.Tensor | DataLoader,c: torch.Tensor | None = None, **train_args):
+        """
+        method to train and save the process
+        Parameters
+        save_path: str
+            where to save the data
+        X : torch.Tensor | DataLoader
+            data to be trained on
+        c: torch.Tensor | None
+            optional conditional tensor
+        **train_args
+            additional arguments to use in training
+        """
         self.train(X=X, c=c, **train_args)
         self.save(path=save_path, training_configs=self.train_configs)
-
 
 class WGANProcess(BaseGanProcess):
     def train(self, X:torch.Tensor, c: torch.Tensor | None = None, **train_args):
