dce6e60298899f8103e54daf3abb8ca8a5844348
ronsperber <85753940+ronsperber@users.noreply.github.com>
2026-04-16 19:34:46 -0400

    added ImageGenerator class

--- src/synthetic_generation/gan/models.py
+++ src/synthetic_generation/gan/models.py
@@ -3,7 +3,7 @@ model with classes for Generator and Discriminator (or Critic)
 for a GAN/WGAN-GP
 """
 
-from typing import Sequence, TypeAlias, List
+from typing import Sequence, TypeAlias
 from dataclasses import dataclass
 from collections.abc import Callable
 import torch
@@ -17,24 +17,195 @@ ActivationFactory: TypeAlias = Callable[[], nn.Module] | nn.Module
 # dataclass for output head info
 @dataclass
 class OutputHead:
-    dim: int                     # output dimension for this head
+    dim: int | tuple[int,...]  # output dimension(s) for this head, tuple is for potential future use
     activation: ActivationFactory = nn.Identity # activation to be applied during training
     decode : ActivationFactory  = nn.Identity # activation to be applied for inference
     name: str = ""               # optional name to identify this head
+
+class ImageGenerator(nn.Module):
+    """
+    class for Generator for images in GAN or WGAN
+    """
+
+    def __init__(
+        self,
+        noise_dim: int,
+        out_dim: tuple[int,...],
+        conv_channels: int | list[int],
+        num_conv_layers: int | None = None,
+        hidden_activation: ActivationFactory = nn.ReLU,
+        output_activation: ActivationFactory = nn.Tanh,
+        conditional_dim: int = 0,
+    ):
+        """
+        initialization for image generation class
+        Parameters
+        ----------
+        noise_dim : int
+            number of noise dimensions to use as inputs
+        out_dim : tuple[int,...]
+            size of output dimensions (C, H, W)
+            note : currently only square images supported
+            resize first if images are not square
+        conv_channels : int | list[int]
+            number of input convolutional channels for first layer or for all layers when a list
+        num_conv_layers : int
+            when conv_channels is an int, the number of layers must be specified
+        hidden_activation : ActivationFactory
+            activation used before output layer
+        output_activation: ActivationFactory
+            activation used for final layer
+        conditional_dim : int
+            when not 0, the dimension of the conditional 
+        """
+        super().__init__()
+        self.noise_dim = noise_dim
+        self.conditional_dim = conditional_dim
+        self.out_dim = out_dim
+        if isinstance(conv_channels, int):
+            if num_conv_layers is None:
+                raise ValueError("Number of convolutional layers must be specified when conv_channels is an int")
+            # when an int is given halve the number of input channels each time
+            conv_channels = [conv_channels // (2 ** n) for n in range(num_conv_layers)]
+        image_size = out_dim[-1] # size of the square that will be output
+        if image_size % (2 ** len(conv_channels)) != 0:
+            raise ValueError(
+                f"image_size {image_size} is not divisible by 2^num_conv_layers "
+                f"(2^{len(conv_channels)}={2 ** len(conv_channels)}). "
+                f"e.g. use num_conv_layers=2 for 28x28 images."
+                )
+        initial_size = image_size // (2 ** len(conv_channels))
+        self.initial_size = initial_size
+        self.initial_channels = conv_channels[0]
+        image_channels = out_dim[0] #number of channels in the image
+        # construct the output channels
+        conv_out_channels = conv_channels[1:] + [image_channels]
+        linear_out_size = conv_channels[0] * initial_size ** 2
+        self.input_layer = nn.Linear(noise_dim + conditional_dim, linear_out_size)
+        self.bn_1 = nn.BatchNorm1d(linear_out_size)
+        self.conv_layers = nn.ModuleList()
+        self.bn_2d = nn.ModuleList()
+        for i, in_channels in enumerate(conv_channels):
+            self.conv_layers.append(
+                nn.ConvTranspose2d(
+                    in_channels=in_channels,
+                    out_channels=conv_out_channels[i],
+                    kernel_size=4,
+                    stride=2,
+                    padding=1)
+                    )
+        for out_channels in conv_out_channels[:-1]:
+            self.bn_2d.append(nn.BatchNorm2d(out_channels))
+        if callable(hidden_activation) and not isinstance(hidden_activation,nn.Module):
+            self.activation = hidden_activation()
+        else:
+            self.activation = hidden_activation
+        if callable(output_activation) and not isinstance(output_activation, nn.Module):
+            self.output_activation = output_activation()
+        else:
+            self.output_activation = output_activation
+    
+    def forward(self, z: torch.Tensor, c: torch.Tensor | None = None) -> torch.Tensor:
+        """
+        forward method for the network
+        Parameters
+        ----------
+        z : torch.Tensor
+            input noise
+        c : torch.Tensor
+            optional conditional tensor if there is a conditional
+        Returns
+        -------
+        torch.Tensor
+            the result of passing (z,c) through the network
+        """
+        if self.conditional_dim > 0 and c is None:
+            raise ValueError(f"Conditional dimension is {self.conditional_dim} and no conditional was passed.")
+        if c is not None:
+            x = torch.cat([z, c], dim = 1)
+        else:
+            x = z
+        x = self.input_layer(x)
+        x = self.bn_1(x)
+        x = self.activation(x)
+        B = x.size(0)
+        x = x.view(B, self.initial_channels, self.initial_size, self.initial_size)
+        for conv, bn in zip(self.conv_layers[:-1], self.bn_2d):
+            x = conv(x)
+            x = bn(x)
+            x = self.activation(x)
+        x = self.conv_layers[-1](x)
+        x = self.output_activation(x)
+        return x
+
+    def generate(self, num_samples: int, c: torch.Tensor | None = None):
+        """
+        method to generate a 'fake' data set
+        Parameters
+        ----------
+        num_samples : int
+            how many samples to be generated
+        c : torch.Tensor | None
+            when not none, the conditional to be used fot generating data
+        Returns
+        -------
+        torch.Tensor
+            a 'fake' dataset of shape (num_samples, *self.output_dim)
+        """
+        device = next(self.parameters()).device
+        # if there is a conditional validate that the length of the conditional
+        # matches the number of samples
+        if c is not None:
+            if c.shape[0] != num_samples:
+                raise ValueError(
+                    "Number of samples must equal length of conditional input"
+                )
+            c = c.to(device)
+        # generate random noise and output data
+        z = torch.randn(num_samples, self.noise_dim, device=device)
+        return self.forward(z, c)
+
+    def generate_samples(self, num_samples: int, c: torch.Tensor | None = None):
+        """
+        function to generate inference samples in eval mode with no gradient tracking
+        Parameters
+        ----------
+        num_samples : int
+            number of samples to generate
+        c : torch.Tensor | None
+            when not None conditional to use to generate the samples
+        Returns
+        -------
+        torch.Tensor
+            sample of size num_samples of sample data with post-training decode used
+        """
+        # save current training status
+        training = self.training
+        # put in eval to use decode on output heads
+        self.eval()
+        with torch.no_grad():
+            # get the output using decoders in output heads
+            output = self.generate(num_samples, c)
+        # restore training state
+        if training:
+            self.train()
+        return output.cpu()
+
+
+
 
 class Generator(nn.Module):
     """
     class for Generator in a GAN or WGAN
     """
 
-  
     def __init__(
         self,
         noise_dim: int,
         num_hidden_layers: int,
         hidden_dims: HiddenDims,
         out_dim: int | None = None,
-        output_heads: List[OutputHead] | None = None,  # list[OutputHead]
+        output_heads: list[OutputHead] | None = None,  
         hidden_activation: ActivationFactory = nn.LeakyReLU,
         conditional_dim: int = 0,
     ):
@@ -127,7 +298,7 @@ class Generator(nn.Module):
                 self.head_decodes.append(head.decode)
 
 
-    def forward(self, z: torch.Tensor, c: torch.Tensor | None = None):
+    def forward(self, z: torch.Tensor, c: torch.Tensor | None = None) -> torch.Tensor :
         """
         forward method for the network
         Parameters
@@ -136,6 +307,10 @@ class Generator(nn.Module):
             input noise
         c : torch.Tensor
             optional conditional tensor if there is a conditional
+        Returns
+        -------
+        torch.Tensor
+            the result of passing (z,c) through the network
         """
         if self.conditional_dim > 0 and c is None:
             raise ValueError(f"Conditional dimension is {self.conditional_dim} and no conditional was passed.")
